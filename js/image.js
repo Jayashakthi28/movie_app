f5f086befb6250c6b297bed0351505f6af871264
Jayashakthi28 <vishnu28.js@gmail.com>
2021-07-15 10:35:19 +0530

    Added Dynamic images for the main poster

---
+++ js/image.js
@@ -0,0 +1,43 @@
+const url =
+  "https://api.themoviedb.org/3/discover/movie?api_key=6c62b994f70846dd9201f6a1f089125e&region=IN&sort_by=popularity.desc";
+const arr = [];
+const url_arr=[];
+const img_arr = fetch(url)
+  .then((data) => data.json())
+  .then((data) => {arr.push(...data.results)
+    img_urls();
+    imgUrlUpdate();
+  });
+function img_urls(){
+    url_arr.push(...arr.map(data=>{return `https://image.tmdb.org/t/p/original${data.poster_path}`}));    
+}
+let count=0;
+function imgUrlUpdate(){
+  const imgs=Array.from(document.querySelectorAll('.main_poster'));
+  imgs.forEach(data => {
+    if(count === url_arr.length){
+      count=0;
+    }
+    if(data.style.display!='none'){
+      data.src=url_arr[count];
+      count++;
+    }
+  })
+}
+function window_Size(){
+  if(window.matchMedia("(max-width: 1600px)").matches){
+    document.querySelector('.img3').style.display='none';
+  }
+  if(window.matchMedia("(min-width: 1600px)").matches){
+    document.querySelector('.img3').style.display='initial';
+  }
+  if(window.matchMedia("(max-width: 900px)").matches){
+    document.querySelector('.img2').style.display='none';
+  }
+  if(window.matchMedia("(min-width: 900px)").matches){
+    document.querySelector('.img2').style.display='initial';
+  }
+}
+setInterval(imgUrlUpdate,5000);
+// var x = window.matchMedia("(max-width: 700px)");
+window.onresize = window.onload = window_Size;
